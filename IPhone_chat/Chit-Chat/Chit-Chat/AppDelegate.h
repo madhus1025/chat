a62035eb8146e8b05a09d8ef2759e3578e04ef31
EMM <emm@EMM.local>
2016-05-28 19:39:11 +0530

    Init Project

---
+++ IPhone_chat/Chit-Chat/Chit-Chat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Chit-Chat
+//
+//  Created by EMM on 28/05/16.
+//  Copyright © 2016 EMM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
